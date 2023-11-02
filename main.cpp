ebcfa4c1ab58fef5b3e9560d7531cfc50ee4fba2
summe <summe@LAPTOP-OULMJUCH>
2023-11-02 15:07:57 +0900

    クラス化enemy,player

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "TextureManager.h"
 #include "WinApp.h"
 
+#include <Player.h>
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WinApp* win = nullptr;
